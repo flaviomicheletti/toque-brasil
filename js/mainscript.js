f7152b692c3543acff066681a6dcbb6353520c43
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2021-08-23 02:31:56 -0300

    slider

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,1 +1,37 @@
-// Slider
+let time = 5000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider .div-img")
+    max = images.length;
+
+function nextImage(){
+
+    images[currentImageIndex].classList.remove('selected');
+
+    currentImageIndex++;
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0;
+
+    images[currentImageIndex].classList.add('selected');
+};
+
+function start(){
+    setInterval(()=>{
+        nextImage()
+    }, time)
+};
+
+window.addEventListener("load", start);
+
+
+
+
+
+
+
+
+
+
+
+
+
